75e22231121d690f02d212fbe5196be8335eccf7
Pappuru Lalitha <lpappuru@gmail.com>
2025-07-05 15:03:10 +0530

    java_fundamentals

--- java_fundamentals/src/java_fundamentals/FCS3.java
+++ java_fundamentals/src/java_fundamentals/FCS3.java
@@ -1,10 +1,17 @@
 package java_fundamentals;
-
+import java.util.Scanner;
 public class FCS3 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+			String s = sc.nextLine();
+			if(s=="") {
+				System.out.println("No Values");
+			}
+			else {
+				String[] words=s.split(" " );
+				String result = String.join("," , words);
+				System.out.println(result);
+			}
+		}
 
 	}
-
-}
